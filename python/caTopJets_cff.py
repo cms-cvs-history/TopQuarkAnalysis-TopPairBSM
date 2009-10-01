e9f458fc6335f942aec0704404a9a240fc0bbcbe
Salvatore Rappoccio <sha1-15b661ca0cbf207b4ae3d8fa8842a4216051970d@cern.ch>
2009-10-01 03:12:23 +0000

    Backport to 3.1.x

--- python/caTopJets_cff.py
+++ python/caTopJets_cff.py
@@ -4,33 +4,23 @@ from RecoJets.JetProducers.CATopJetParameters_cfi import *
 from RecoJets.JetProducers.GenJetParameters_cfi import *
 from RecoJets.JetProducers.CaloJetParameters_cfi import *
 from RecoJets.JetProducers.PFJetParameters_cfi import *
-from RecoJets.JetProducers.AnomalousCellParameters_cfi import *
 
 
 caTopCaloJets = cms.EDProducer("CATopJetProducer",
                                CATopJetParameters,
-                               CaloJetParameters,
-                               AnomalousCellParameters,
-                               jetAlgorithm = cms.string("CambridgeAachen"),
-                               rParam = cms.double(0.8)
+                               CaloJetParameters
                                )
 
 
 caTopGenJets = cms.EDProducer("CATopJetProducer",
                               CATopJetParameters,
-                              GenJetParameters,
-                              AnomalousCellParameters,
-                              jetAlgorithm = cms.string("CambridgeAachen"),
-                              rParam = cms.double(0.8)
+                              GenJetParameters
                               )
 
 
 
 caTopPFJets = cms.EDProducer("CATopJetProducer",
                              CATopJetParameters,
-                             PFJetParameters,
-                             AnomalousCellParameters,
-                             jetAlgorithm = cms.string("CambridgeAachen"),
-                             rParam = cms.double(0.8)
+                             PFJetParameters
                              )
 
