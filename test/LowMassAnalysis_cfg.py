d80bff66df95b96ceff69759d1b27bb117b8768d
Geng-Yuan Jeng <sha1-22504dd22d9623ddee36a32f0ec012f4644ca431@cern.ch>
2010-02-16 03:06:02 +0000

    Update

--- test/LowMassAnalysis_cfg.py
+++ test/LowMassAnalysis_cfg.py
@@ -8,19 +8,12 @@ process = cms.Process("LowMass")
 ## add message logger
 process.load("FWCore.MessageLogger.MessageLogger_cfi")
 process.MessageLogger.cerr.threshold = 'INFO'
+process.MessageLogger.cerr.FwkReport.reportEvery = 1000
 
 ## configure process options
 process.options = cms.untracked.PSet(
         wantSummary = cms.untracked.bool(True)
         )
-
-## configure geometry
-process.load("Configuration.StandardSequences.Geometry_cff")
-## configure conditions
-process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
-process.GlobalTag.globaltag = cms.string('MC_31X_V2::All')
-## load magnetic field
-process.load("Configuration.StandardSequences.MagneticField_cff")
 
 # load sequences
 process.load("TopQuarkAnalysis.TopPairBSM.TopAnalysis_sequences")
@@ -29,20 +22,20 @@ process.load("TopQuarkAnalysis.TopPairBSM.TopAnalysis_sequences")
 #process.p = cms.Path( process.TopAnalysisMuFilter ) # with muonic generator filter
 #process.p = cms.Path( process.TopAnalysisNoMuFilter ) # with generator filter on all but muonci decays
 process.p = cms.Path( process.TopAnalysis ) # no generator filter at all
-process.TopAnalyzer.debug = cms.bool(True)
-#process.TopAnalyzer.jetSource      = cms.InputTag('selectedLayer1JetsJPT')
+#process.TopAnalyzer.debug = cms.bool(True)
+#process.TopAnalyzer.jetSource = cms.InputTag('selectedLayer1JetsJPT')
 
 # change defaults
-process.BooTopHLTFilter.HLTPaths = cms.vstring() # do not filter
+#process.BooTopHLTFilter.HLTPaths = cms.vstring() # do not filter
+process.BooTopHLTFilter.HLTPaths = [] # do not filter
 
 # source
 process.source = cms.Source("PoolSource",
                             fileNames = cms.untracked.vstring(
-    #'file:testPatTuple.root'
-    'file:TTJets_madgraph_Summer09.root'
+        '/store/user/samvel/TTbarJets-madgraph/TestNewConfig/f13d40777b0f751c898732fbb2354135/ljmet_1.root'
     )
                             )
 
 process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(-1) )
 
-    
+
