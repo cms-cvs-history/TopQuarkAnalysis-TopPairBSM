ec402782e0a814bfc1136415ee18f7e93215015a
Salvatore Rappoccio <sha1-15b661ca0cbf207b4ae3d8fa8842a4216051970d@cern.ch>
2012-06-08 14:40:50 +0000

    Adding b-tagging information on the mass-drop-filtered subjets, and genjets for CA12 mass-drop-filtered

--- test/ttbsm_cfg.py
+++ test/ttbsm_cfg.py
@@ -613,6 +613,14 @@ process.caFilteredGenJetsNoNu = process.ca8GenJetsNoNu.clone(
 	jetPtMin = cms.double(100.0)
 )
 
+process.caMassDropFilteredGenJetsNoNu = process.caFilteredGenJetsNoNu.clone(
+        src = cms.InputTag('genParticlesForJetsNoNu'),
+	useMassDropTagger = cms.bool(True),
+	muCut = cms.double(0.667),
+	yCut = cms.double(0.08)
+)
+
+
 
 ###############################
 #### CATopTag Setup ###########
@@ -788,6 +796,20 @@ if options.useExtraJetColls:
 			 doL1Cleaning=False,
 			 doL1Counters=False,
 			 genJetCollection = cms.InputTag("ca8GenJetsNoNu"),
+			 doJetID = False
+			 )
+
+
+	addJetCollection(process, 
+			 cms.InputTag('caMassDropFilteredPFlow', 'SubJets'),         # Jet collection; must be already in the event when patLayer0 sequence is executed
+			 'CA12MassDropFilteredSubjets', 'PF',
+			 doJTA=True,            # Run Jet-Track association & JetCharge
+			 doBTagging=True,       # Run b-tagging
+			 jetCorrLabel=None,
+			 doType1MET=False,
+			 doL1Cleaning=False,
+			 doL1Counters=False,
+			 genJetCollection = cms.InputTag("ak5GenJetsNoNu"),
 			 doJetID = False
 			 )
 
@@ -1354,6 +1376,19 @@ if options.useExtraJetColls:
 		src = cms.InputTag('ak7FilteredGenJetsNoNu')
 		)
 
+        process.ca8PrunedGenLite = process.ak7TrimmedGenLite.clone(
+                src = cms.InputTag('caPrunedGen')
+                )
+
+        process.ca12FilteredGenLite = process.ak7TrimmedGenLite.clone(
+                src = cms.InputTag('caFilteredGenJetsNoNu')
+                )
+
+        process.ca12MassDropFilteredGenLite = process.ak7TrimmedGenLite.clone(
+                src = cms.InputTag('caMassDropFilteredGenJetsNoNu')
+                )
+
+
 
 	process.ak8Lite = process.ak5Lite.clone(
 		src = cms.InputTag('goodPatJetsAK8PF')
@@ -1383,6 +1418,7 @@ process.patseq = cms.Sequence(
     process.ca8GenJetsNoNu*
     process.ak8GenJetsNoNu*
     process.caFilteredGenJetsNoNu*
+    process.caMassDropFilteredGenJetsNoNu*
     getattr(process,"patPF2PATSequence"+postfix)*
     process.looseLeptonSequence*
     process.patDefaultSequence*
@@ -1429,7 +1465,10 @@ if options.useExtraJetColls:
 		process.ak8Lite*
 		process.ak8TrimmedLite*
 		process.ak8FilteredLite*
-		process.ak8PrunedLite
+		process.ak8PrunedLite*
+                process.ca8PrunedGenLite*
+                process.ca12FilteredGenLite*
+                process.ca12MassDropFilteredGenLite
 	)
 	process.patseq *= process.extraJetSeq
 
@@ -1457,6 +1496,9 @@ if options.useData == True :
 	    process.patseq.remove( process.ak7TrimmedGenLite )
 	    process.patseq.remove( process.ak7FilteredGenLite )
 	    process.patseq.remove( process.ak7PrunedGenLite )
+            process.patseq.remove( process.ca8PrunedGenLite )
+            process.patseq.remove( process.ca12FilteredGenLite )
+            process.patseq.remove( process.ca12MassDropFilteredGenLite )
 
 
 if options.writeSimpleInputs :
@@ -1517,6 +1559,7 @@ process.out.outputCommands = [
     'keep *_selectedPat*_*_*',
     'keep *_goodPat*_*_*',
     'drop patJets_selectedPat*_*_*',
+    'keep patJets_selectedPatJetsCA12MassDropFilteredSubjetsPF*_*_*',
     'drop *_selectedPatJets_*_*',    
     'keep *_patMETs*_*_*',
 #    'keep *_offlinePrimaryVertices*_*_*',
